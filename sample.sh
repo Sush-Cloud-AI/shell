b3c2fafe0ab943081a3a69de3d77c62781df20c1
Sush <sushcloud12@gmail.com>
2023-03-20 18:22:40 +0000

    i

--- sample.sh
+++ sample.sh
@@ -17,3 +17,6 @@ echo -e "\e[32mI am Green\e[0m"
 echo -e "\e[33mI am yellow\e[0m"
 echo -e "\e[34mI am Blue\e[0m"
 echo -e "\e[35mI am Maganta\e[0m"
+
+# forefround clour
+echo -e "\e[45;33mI am yellow\e[0m"
