483c6242b7941a36f4aaa4f7e52e5d3471e74c73
Sush <sushcloud12@gmail.com>
2023-03-20 18:20:57 +0000

    i

--- sample.sh
+++ sample.sh
@@ -10,4 +10,10 @@ echo "HI"
 
 echo -e "Hello\n\tmonkey"
 
+#colour fore ground
 
+echo -e "\e[31mI am Red\e[0m"
+echo -e "\e[32mI am Green\e[0m"
+echo -e "\e[33mI am yellow\e[0m"
+echo -e "\e[34mI am Blue\e[0m"
+echo -e "\e[35mI am Maganta\e[0m"
