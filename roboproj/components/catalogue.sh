43e0402672d9b0eeeaec7f18561b712b22ed7272
Sush <sushcloud12@gmail.com>
2023-03-21 20:25:14 +0000

    i

--- roboproj/components/catalogue.sh
+++ roboproj/components/catalogue.sh
@@ -7,47 +7,19 @@ COMPONENT=catalogue
 LOG_FILE="/tmp/$COMPONENT.log"
 APP_USER=roboshop
 
-COMPONENT_URL="https://github.com/stans-robot-project/catalogue/archive/main.zip"
+
 source components/common.sh   # validating if its a root user in common.sh
 
-echo -n "configuring NodeJS Repo:  "
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOG_FILE
-stat $?
+# CALLING NODE JS FUNCTION FROM COMMON FILE 
+NODEJS
 
-echo -n "Inastalling NodeJS: "
-yum install nodejs -y  &>> $LOG_FILE
-stat $?
-
-
-echo -n "Creating roboshop user: "
-id roboshop &>> $LOG_FILE || useradd $APP_USER
-stat $?
-
-echo -n "Download the  $COMPONENT code: "
-curl -s -L -o /tmp/catalogue.zip $COMPONENT_URL  &>> $LOG_FILE
-stat $?
-
-echo -n "Performing cleanup: "
-cd /home/${APP_USER}/ && rm -rf ${COMPONENT} &>> $LOG_FILE
-stat $?
-
-echo -n "Extracting the $COMPONENT: "
-cd /home/roboshop
-unzip -o /tmp/${COMPONENT}.zip &>> $LOG_FILE
-mv ${COMPONENT}-main ${COMPONENT} && chown -R $APP_USER:$APP_USER $COMPONENT
-cd ${COMPONENT}
-stat $?
-
-echo -n "Installing the $COMPONENT: "
-npm install &>> $LOG_FILE
-stat $?
 
 echo -n "Configuring the service: "
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' systemd.service
 mv /home/${APP_USER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 stat $?
 
-service_start    # service start function
+SERVICE_START   # service start function
 
 
 echo -e -------------- "\e[33m $COMPONENT configuration completed. \e[0m"--------------------
--- roboproj/components/common.sh
+++ roboproj/components/common.sh
@@ -2,6 +2,9 @@
 
 # validating if the user is a root to execute the commands
 
+COMPONENT_URL="https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+
+
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[31m You may need to run it as a root user only. \e[0m"
@@ -17,7 +20,63 @@ stat (){
     fi
 }
 
-service_start(){
+
+
+NODEJS(){
+    echo -n "configuring NodeJS Repo:  "
+    curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOG_FILE
+    stat $?
+
+    echo -n "Inastalling NodeJS: "
+    yum install nodejs -y  &>> $LOG_FILE
+    stat $
+
+    USER_CREA
+
+    REPO_DOWN
+
+    CLEAN_UP
+
+    EXTRACT_COMP
+
+    INSTALL_COMP
+}
+
+USER_CREA(){
+    echo -n "Creating roboshop user: "
+    id roboshop &>> $LOG_FILE || useradd $APP_USER
+    stat $?
+}
+
+REPO_DOWN(){
+    echo -n "Download the  $COMPONENT REPO: "
+    curl -s -L -o /tmp/$COMPONENT.zip $COMPONENT_URL  &>> $LOG_FILE
+    stat $?
+}
+
+CLEAN_UP(){
+   echo -n "Performing cleanup: "
+    cd /home/${APP_USER}/ && rm -rf ${COMPONENT} &>> $LOG_FILE
+    stat $? 
+}
+
+EXTRACT_COMP(){
+    echo -n "Extracting the $COMPONENT: "
+    cd /home/roboshop
+    unzip -o /tmp/${COMPONENT}.zip &>> $LOG_FILE
+    mv ${COMPONENT}-main ${COMPONENT} && chown -R $APP_USER:$APP_USER $COMPONENT
+    cd ${COMPONENT}
+    stat $?
+}
+
+INSTALL_COMP{
+    echo -n "Installing the $COMPONENT: "
+    npm install &>> $LOG_FILE
+    stat $?
+}
+
+
+SERVICE_START(){
     echo -n "Starting $COMPONENT service: "
     systemctl daemon-reload &>> $LOG_FILE
     systemctl restart $COMPONENT &>> $LOG_FILE
