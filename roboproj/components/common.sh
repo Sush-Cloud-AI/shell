64f4729a052146c848a0d09cf7803bc9d2cb893f
Sush <sushcloud12@gmail.com>
2023-03-21 20:45:54 +0000

    i

--- roboproj/components/common.sh
+++ roboproj/components/common.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# validating if the user is a root to execute the commands
 
 COMPONENT_URL="https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 
-
+# validating if the user is a root to execute the commands
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[31m You may need to run it as a root user only. \e[0m"
---
+++ roboproj/components/redis.sh
@@ -0,0 +1,28 @@
+set -e # will exit the script if there is an error
+
+source components/common.sh
+
+COMPONENT=redis
+LOG_FILE="/tmp/$COMPONENT.log"
+APP_USER=roboshop
+
+REDIS_REPO_URL="https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo"
+
+
+
+echo -n "Downloading $COMPONENT repo: "
+curl -s -o $REDIS_REPO_URL /etc/yum.repos.d/redis.repo  &>> $LOG_FILE
+stat $?
+
+echo -n "Inastalling  $COMPONENT: "
+yum install redis-6.2.11 -y  &>> $LOG_FILE
+stat $?
+
+echo -n "Upadting $COMPONENT Listening address: "
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf ## -i should be followed by -e
+stat $?
+
+SERVICE_START
+
+
+echo -e -------------- "\e[33m $COMPONENT configuration completed. \e[0m"--------------------
