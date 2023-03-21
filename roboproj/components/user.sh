375f6dfb359ca04019e1f85d6d437e5b9292db0f
Sush <sushcloud12@gmail.com>
2023-03-21 21:43:46 +0000

    i

--- roboproj/components/user.sh
+++ roboproj/components/user.sh
@@ -14,13 +14,24 @@ APP_USER=roboshop
 # CALLING NODE JS FUNCTION FROM COMMON FILE 
 NODEJS
 
+USER_CREA
 
-echo -n "Configuring the service: "
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' systemd.service
-mv /home/${APP_USER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-stat $?
+REPO_DOWN
 
-SERVICE_START   # service start function
+CLEAN_UP
+
+#EXTRACT_COMP
+
+#INSTALL_COMP
 
 
-echo -e -------------- "\e[33m $COMPONENT configuration completed. \e[0m"--------------------
+
+#echo -n "Configuring the service: "
+#sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' systemd.service
+#mv /home/${APP_USER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+#stat $?
+
+#SERVICE_START   # service start function
+
+
+#echo -e -------------- "\e[33m $COMPONENT configuration completed. \e[0m"--------------------
