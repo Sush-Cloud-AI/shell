9e03c14e1c978f5f012aba0a762d638e6fe6d426
Sush <sushcloud12@gmail.com>
2023-03-21 07:52:30 +0000

    i

--- roboproj/components/frontend.sh
+++ roboproj/components/frontend.sh
@@ -6,13 +6,7 @@ LOG_FILE="/tmp/$COMPONENT.log"
 
 source components/common.sh   # validating if its a root user in common.sh
 
-stat (){
-    if [ $1 -eq 0 ] ; then
-        echo -e  "\e[32m SUCCESS. \e[0m"
-    else
-        echo -e  "\e[31m FAILURE. \e[0m"
-    fi
-}
+
 
 echo -n "Installing Nginx: "  # -n : cursor wont move to the next line
 yum install nginx -y  &>> $LOG_FILE
