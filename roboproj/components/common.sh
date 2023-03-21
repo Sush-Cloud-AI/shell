4fb98a13445210efc8690fa5ccd36b06560df72f
Sush <sushcloud12@gmail.com>
2023-03-21 07:49:18 +0000

    i

--- roboproj/components/common.sh
+++ roboproj/components/common.sh
@@ -7,3 +7,12 @@ if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[31m You may need to run it as a root user only. \e[0m"
     exit 1
 fi
+
+
+stat (){
+    if [ $1 -eq 0 ] ; then
+        echo -e  "\e[32m SUCCESS. \e[0m"
+    else
+        echo -e  "\e[31m FAILURE. \e[0m"
+    fi
+}
--- roboproj/components/frontend.sh
+++ roboproj/components/frontend.sh
@@ -6,25 +6,44 @@ LOG_FILE="/tmp/$COMPONENT.log"
 
 source components/common.sh   # validating if its a root user in common.sh
 
+stat (){
+    if [ $1 -eq 0 ] ; then
+        echo -e  "\e[32m SUCCESS. \e[0m"
+    else
+        echo -e  "\e[31m FAILURE. \e[0m"
+    fi
+}
+
 echo -n "Installing Nginx: "  # -n : cursor wont move to the next line
 yum install nginx -y  &>> $LOG_FILE
+stat $?
 
-if [ $? -eq 0 ] ; then
-    echo -e  "\e[32m SUCCESS. \e[0m"
-else
-    echo -e  "\e[31m FAILURE. \e[0m"
-fi
-# systemctl enable nginx
-# systemctl start nginx
 
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl enable nginx &>> $LOG_FILE
 
-# systemctl restart nginx
+echo -n "Starting Nginx: "
+systemctl start nginx &>> $LOG_FILE
+stat $?
+
+echo -n "Downloading the Repo: "
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> $LOG_FILE
+stat $?
+
+echo -n "Clearing the old content: "
+cd /usr/share/nginx/html
+rm -rf *
+stat $?
+
+echo -n "Extracting the $COMPONENT: "
+unzip /tmp/frontend.zip
+stat $?
+
+echo -n "Updating the Proxy file: "
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo -e -------------- "\e[31m $COMPONENT configuration. \e[0m"--------------------
