a51b474baae1b937d26c165e1a73b61e8d2696eb
Sush <sushcloud12@gmail.com>
2023-04-01 16:57:07 +0100

    i

--- roboproj/components/frontend.sh
+++ roboproj/components/frontend.sh
@@ -41,7 +41,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 echo -n "configure the proxy file: "
-sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' -e '/user/s/localhost/user.roboshop.internal/' -e '/cart/s/localhost/cart.roboshop.internal/' -e '/shipping/s/localhost/shipping.roboshop.internal/' -e '/payment/s/localhost/payment.roboshop.internal/' robohsop.conf
+sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' -e '/user/s/localhost/user.roboshop.internal/' -e '/cart/s/localhost/cart.roboshop.internal/' -e '/shipping/s/localhost/shipping.roboshop.internal/' -e '/payment/s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 stat $?
 
 echo -n "restarting Ngnix: "
