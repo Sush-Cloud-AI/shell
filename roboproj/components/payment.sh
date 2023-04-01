7439c6f884d26d1231b8f1c57302bbbffd2dfcc8
Sush <sushcloud12@gmail.com>
2023-04-01 10:26:13 +0100

    i

--- roboproj/components/payment.sh
+++ roboproj/components/payment.sh
@@ -27,9 +27,14 @@ EXTRACT_COM(){
     stat $?
 }
 EXTRACT_COM
+
 echo -n "Installing dependencies: "
 cd /home/roboshop/payment 
 pip3 install -r requirements.txt &>> $LOG_FILE
 stat $?
 
-#sed -i -e '
+echo -n "Updating the $COMPONENT.ini file: "
+USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
+sed -i -e "/uid/ c uid = $USER_ID" -e "/gid/ c gid = $GROUP_ID" payment.ini
+stat $?
