0872d3b1cac0f4b67d8e45f6d0b61cb686ac3ccf
Sush <sushcloud12@gmail.com>
2023-04-01 20:33:57 +0100

    i

--- roboproj/create-server.sh
+++ roboproj/create-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-AMI_ID =$(aws ec2 describe-images --filters "Name=name,Values=devops" | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=devops" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
