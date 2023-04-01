3c4aaf68b1288d9550cc6c710c7d30476a43515d
Sush <sushcloud12@gmail.com>
2023-04-01 04:59:37 +0100

    i

--- roboproj/components/mysql.sh
+++ roboproj/components/mysql.sh
@@ -1,11 +1,11 @@
 COMPONENT=mysql
 LOG_FILE="/tmp/$COMPONENT.log"
 MYSQL_REPO_URL=" https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo"
-COMPONENT_URL="https://github.com/stans-robot-project/mongodb/archive/main.zip"
+#COMPONENT_URL="https://github.com/stans-robot-project/mongodb/archive/main.zip"
 source components/common.sh   # validating if its a root user in common.sh
 
 echo -n "Downloading $COMPONENT repo: "
-curl -s -o /etc/yum.repos.d/mongodb.repo $MYSQL_REPO_URL &>> $LOG_FILE
+curl -s -o /etc/yum.repos.d/mysql.repo $MYSQL_REPO_URL &>> $LOG_FILE
 stat $?
 
 echo -n "Inastalling  $COMPONENT: "
