3549a2f12882621bfcc41a4c5304ccdb5843800e
Sush <sushcloud12@gmail.com>
2023-04-02 07:26:01 +0100

    i

--- roboproj/create-server.sh
+++ roboproj/create-server.sh
@@ -2,10 +2,25 @@
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=devops" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
 COMPONENT=$1
-PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type t3.micro  --security-group-ids sg-03ec6d3f23f96aefd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g') 
-echo $PRIVATE_IP
 
 
-##ip creation
-sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > /tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z05313022PGNHL8COSD1E --change-batch file:///tmp/record.json | jq
+SERVER_CREATE(){
+    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type t3.micro  --security-group-ids sg-03ec6d3f23f96aefd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g') 
+    echo $PRIVATE_IP
+
+
+    ##ip creation
+    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > /tmp/record.json
+    aws route53 change-resource-record-sets --hosted-zone-id Z05313022PGNHL8COSD1E --change-batch file:///tmp/record.json | jq
+
+
+}
+
+if [ "$1" == "all" ] ; then
+    for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis ; do
+        COMPONENT=$component
+        SERVER_CREATE
+    done
+else
+    SERVER_CREATE
+fi
