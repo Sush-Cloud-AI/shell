4d4286be7ec3b2e9f87bf5c18feec91d8335d3b1
Sush <sushcloud12@gmail.com>
2023-04-02 06:16:37 +0100

    i

--- roboproj/create-server.sh
+++ roboproj/create-server.sh
@@ -2,4 +2,4 @@
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=devops" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
 COMPONENT=$1
-aws ec2 run-instances --image-id $AMI_ID  --instance-type t3.micro  --security-group-ids sg-03ec6d3f23f96aefd --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${COMPONENT}}]" | jq
+aws ec2 run-instances --image-id $AMI_ID  --instance-type t3.micro  --security-group-ids sg-03ec6d3f23f96aefd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
